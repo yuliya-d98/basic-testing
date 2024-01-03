db5bba90ccbcf24f95471ad5939a5f6628a9d034
Yuliya Dubovik <feelgloom@gmail.com>
2024-01-03 23:36:26 +0300

    feat: simple tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,74 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const expected = 19;
+    const result = simpleCalculator({
+      a: 7,
+      b: 12,
+      action: Action.Add,
+    });
+    expect(result).toBe(expected);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const expected = 19;
+    const result = simpleCalculator({
+      a: 33,
+      b: 14,
+      action: Action.Subtract,
+    });
+    expect(result).toBe(expected);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const expected = 33;
+    const result = simpleCalculator({
+      a: 3,
+      b: 11,
+      action: Action.Multiply,
+    });
+    expect(result).toBe(expected);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const expected = 33;
+    const result = simpleCalculator({
+      a: 66,
+      b: 2,
+      action: Action.Divide,
+    });
+    expect(result).toBe(expected);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const expected = 8;
+    const result = simpleCalculator({
+      a: 2,
+      b: 3,
+      action: Action.Exponentiate,
+    });
+    expect(result).toBe(expected);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const expected = null;
+    const result = simpleCalculator({
+      a: 2,
+      b: 3,
+      action: 78,
+    });
+    expect(result).toBe(expected);
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const expected = null;
+    const result = simpleCalculator({
+      a: 'aaaaaaaaa',
+      b: 'lalalal',
+      action: Action.Exponentiate,
+    });
+    expect(result).toBe(expected);
   });
 });
