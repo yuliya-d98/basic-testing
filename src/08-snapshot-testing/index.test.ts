0496ef5df39437c02caee0d6e215b097f34e36e1
Yuliya Dubovik <feelgloom@gmail.com>
2024-01-29 22:58:28 +0300

    feat: snapshot testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,34 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
+// https://jestjs.io/docs/snapshot-testing
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const result = generateLinkedList<number>([1, 1, 1, 1, 1]);
+    expect(result).toStrictEqual({
+      value: 1,
+      next: {
+        value: 1,
+        next: {
+          value: 1,
+          next: {
+            value: 1,
+            next: {
+              value: 1,
+              next: {
+                value: null,
+                next: null,
+              },
+            },
+          },
+        },
+      },
+    });
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const result = generateLinkedList<number>([2, 2, 2, 2]);
+    expect(result).toMatchSnapshot();
   });
 });
